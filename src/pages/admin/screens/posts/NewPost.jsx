d0cf03169907f7f031c0c83b3ee44112b79f6267
zaiyab <zaiyabdati@gmail.com>
2023-08-27 22:26:26 +0500

    newpost functionality added

--- src/pages/admin/screens/posts/NewPost.jsx
+++ src/pages/admin/screens/posts/NewPost.jsx
@@ -7,21 +7,26 @@ import { toast } from "react-hot-toast";
 const NewPost = () => {
   const userState = useSelector((state) => state.user);
 
-  const [title,setTitle] = useState();
-  const handletitle = (v)=>{
+  const [title, setTitle] = useState();
+  const handletitle = (v) => {
     setTitle(v.target.value)
   }
-  const [desc,setDesc] = useState();
-  const handledesc = (v)=>{
+  const [desc, setDesc] = useState();
+  const handledesc = (v) => {
     setDesc(v.target.value)
   }
-  const handleclear = ()=>{
+  const handleclear = () => {
     setTitle("");
     setDesc('');
   }
-  const handlesubmit =async (e)=>{
+  const handlesubmit = async (e) => {
     e.preventDefault();
     try {
+      if (!title || !desc) {
+        toast.error("Title and description are required.");
+        return;
+      }
+
       const url = '/api/posts'; // Replace with your actual URL
       const token = userState.userInfo.token;
 
@@ -31,42 +36,58 @@ const NewPost = () => {
       };
 
       const postData = {
-      title:title,
-      caption:desc
+        title: title,
+        caption: desc
       };
 
       const response = await axios.post(url, postData, { headers });
- if(response){
-  toast.success("Post is added");
- }
+      if (response.status === 200) {
+        toast.success("Post is added");
+      }
+
     } catch (error) {
-    if (error.response && error.response.data.message)
+      toast.error("Something went wrong");
+      if (error.response && error.response.data.message)
         throw new Error(error.response.data.message);
+
+
       throw new Error(error.message);
+
     }
-    
+
   }
 
-  
+
 
   return (
     <form onSubmit={handlesubmit}>
-    <div>
-      <div className="heading text-center font-bold text-2xl m-5 text-gray-800">New Post</div>
+      <div>
+        <div className="heading text-center font-bold text-2xl m-5 text-gray-800">New Post</div>
 
-  <div className="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
-    <input value={title} onChange={handletitle} className="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="false" placeholder="Title" type="text" />
-    <textarea value={desc} onChange={handledesc} className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" placeholder="Describe everything about this post here"></textarea>
-    
-
+        <div className="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
+          <input value={title} onChange={handletitle} className="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="false" placeholder="Title" type="text" />
+          <textarea value={desc} onChange={handledesc} className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" placeholder="Describe everything about this post here"></textarea>
 
 
-    <div className="buttons flex mt-2">
-      <div onClick={handleclear}  className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto">Cancel</div>
-      <button type='submit'><div o className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Post</div></button>
-    </div>
-  </div>
-    </div>
+
+          <div className="buttons flex mt-2">
+            <button
+              onClick={handleclear}
+              className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto"
+              type="button" // This is a regular button, not for form submission
+            >
+              Cancel
+            </button>
+            <button
+              className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500"
+              type="submit" // This button will submit the form
+            >
+              Post
+            </button>
+          </div>
+
+        </div>
+      </div>
     </form>
   )
 }
